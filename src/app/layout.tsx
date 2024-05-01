919f8f2bacaf3538189e938b2f2adff68f59a56e
Luis Fernando <guffenix@gmail.com>
2024-05-01 01:08:33 -0500

    chore: images config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from 'next'
 import './globals.css'
 import { ThemeProvider } from '~/components/theme-provider'
 import { HeaderSection } from '~/components/sections/header.section'
-import { PushNavigationMenu } from '~/components/site/header'
 
 export const metadata: Metadata = {
   title: 'Embajadores | Push (Previously EPNS)',
