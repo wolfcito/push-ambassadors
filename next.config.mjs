0d41352b9da4ad04ee3aca5143f59dc2c6304c04
Luis Fernando <guffenix@gmail.com>
2024-05-01 01:19:01 -0500

    feat: remote patterns adde

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/guffenix/image/upload/**',
+      },
+    ],
+  },
 }
 
 export default nextConfig
